52eaaa834f1b25bea2693ed7651384a960badc97
Categio <categio1@gmail.com>
2023-01-26 16:44:33 -0800

    Add project files.

---
+++ Cell Phone Test/CellPhone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cell_Phone_Test
+{
+    class CellPhone
+    {
+        //Constructor 
+        public CellPhone()
+        {
+            Brand = "";
+            Model = "";
+            Price = "0m";
+        }
+
+        //Brand property getter/setter
+        public string Brand { get; set; }
+
+        // Model property getter/Setter
+        public string Model { get; set; }
+
+        //Price property getter/setter
+        public decimal Price { get; set; }
+
+    }
+}
